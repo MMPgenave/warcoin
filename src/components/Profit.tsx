f90235ae9bcba0d60f59118c48530366a1308c2a
MMPgenave <mohammadmp14@gmail.com>
2025-01-31 02:08:49 +0330

    fix Profit component

--- src/components/Profit.tsx
+++ src/components/Profit.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useSelector, useDispatch } from "react-redux";
 import { setUserWarcoin, setProfit } from "@/features/Trade/TradeSlice";
@@ -23,6 +24,7 @@ const Profit = () => {
     dispatch(setUserWarcoin(newUserCoins));
     dispatch(setProfit(profit));
   }, []);
+
   return (
     <div className=" w-full absolute -left-[13rem] top-1/2 -translate-y-1/2">
       <Dialog>
@@ -71,3 +73,23 @@ const Profit = () => {
 };
 
 export default Profit;
+
+// const VariableDialogContent = ({ imgUrl, tradeType, bet, profit }) => {
+//   return (
+//     <DialogContent className={`w-full bg-cover bg-center h-screen ${imgUrl}`}>
+//       <DialogHeader>
+//         <div className="flex items-center gap-10">
+//           <div>
+//             <DialogTitle className=" text-3xl font-bold text-slate-100">Bitcoin</DialogTitle>
+//             <div className="flex items-center gap-6 text-2xl ">
+//               <div className="text-mmpred">{tradeType}</div>
+//               <div className="   text-[#a49c9c]">3x</div>
+//             </div>
+//             <div className=" text-2xl text-slate-100">Amt:{bet}</div>
+//           </div>
+//           <div className="text-[3.5rem] text-mmpred">{profit}%</div>
+//         </div>
+//       </DialogHeader>
+//     </DialogContent>
+//   );
+// };
