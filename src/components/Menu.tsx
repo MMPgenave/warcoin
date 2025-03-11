ea3008099c1affbb47c746c8e2249ad66318a753
MMPgenave <mohammadmp14@gmail.com>
2025-03-11 21:49:51 +0330

    menubar updated

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,10 @@
 import { Link } from "@/components/Link/Link.tsx";
 import { routes } from "@/navigation/routes";
-
+import { useLocation } from "react-router-dom";
 const Menu = () => {
+  const location = useLocation();
   return (
-    <div className=" fixed bottom-0 flex justify-between  w-full px-[1rem] pb-1 z-10">
+    <div className=" fixed bottom-0 flex justify-between  w-full px-[2.5rem] pb-3 z-10 ">
       {routes.map((route) => (
         <Link
           to={route.path}
@@ -11,8 +12,15 @@ const Menu = () => {
           className=" flex flex-col gap-0 justify-center items-center text-[#fff] text-[1.3rem]
          "
         >
-          <i className={`bi ${route.icon}  `}></i>
-          <div>{route.title}</div>
+          {/* <i className={`bi ${route.icon}  `}></i> */}
+          <img src={route.path === location.pathname ? route.imgActive : route.imgInActive} alt="" />
+          <div
+            className={`text-[10px] font-normal  ${
+              route.path === location.pathname ? "text-[#ffffff]" : "text-[#ffffff]/30"
+            }`}
+          >
+            {route.title}
+          </div>
         </Link>
       ))}
     </div>
--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -5,12 +5,24 @@ import { Task } from "@/pages/Task";
 import { Friend } from "@/pages/Friend";
 import { Trade } from "@/pages/Trade";
 import { Wallet } from "@/pages/TONConnectPage/Wallet";
+import homeActive from "@/mmpassets/home-active.png";
+import homeInActive from "@/mmpassets/home-inactive.png";
+import taskAcitve from "@/mmpassets/task-active.png";
+import taskInAcitve from "@/mmpassets/task-inactive.png";
+import tradeAcitve from "@/mmpassets/trade-active.png";
+import tradeInAcitve from "@/mmpassets/trade-inactive.png";
+import friendsAcitve from "@/mmpassets/friends-active.png";
+import friendsInAcitve from "@/mmpassets/friends-inactive.png";
+import walletAcitve from "@/mmpassets/wallet-active.png";
+import walletInAcitve from "@/mmpassets/wallet-inactive.png";
 
 interface Route {
   path: string;
   Component: ComponentType;
   title?: string;
   icon?: string;
+  imgActive: string;
+  imgInActive: string;
 }
 
 export const routes: Route[] = [
@@ -19,14 +31,39 @@ export const routes: Route[] = [
     Component: IndexPage,
     title: "Home",
     icon: "bi-house",
+    imgActive: homeActive,
+    imgInActive: homeInActive,
   },
-  { path: "/task", Component: Task, title: "Task", icon: "bi-list-task" },
-  { path: "/trade", Component: Trade, title: "Trade", icon: "bi-graph-up-arrow" },
-  { path: "/friend", Component: Friend, title: "Friend", icon: "bi-people" },
+  {
+    path: "/task",
+    Component: Task,
+    title: "Task",
+    icon: "bi-list-task",
+    imgActive: taskAcitve,
+    imgInActive: taskInAcitve,
+  },
+  {
+    path: "/trade",
+    Component: Trade,
+    title: "Trade",
+    icon: "bi-graph-up-arrow",
+    imgActive: tradeAcitve,
+    imgInActive: tradeInAcitve,
+  },
+  {
+    path: "/friend",
+    Component: Friend,
+    title: "Friends",
+    icon: "bi-people",
+    imgActive: friendsAcitve,
+    imgInActive: friendsInAcitve,
+  },
   {
     path: "/ton-connect",
     Component: Wallet,
     title: "Wallet",
     icon: "bi-wallet2",
+    imgActive: walletAcitve,
+    imgInActive: walletInAcitve,
   },
 ];
